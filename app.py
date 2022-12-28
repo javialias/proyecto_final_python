673aeb246a4292433c577dc5dda672769f8dc68a
javialias <95072118+javialias@users.noreply.github.com>
2022-12-28 09:07:31 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import crypto_pair as cp
+
+"""
+# Proyecto final Python
+##### Samuel Medina Gutiérrez y Francisco Javier Alías Carrascosa
+
+Este es el informe sobre el proyecto de la asignatura _Python para el análisis de Datos_ del
+Máster en Big Data Science de la Universidad de Navarra. 
+
+1. Puedo escribir enumeraciones
+2. Como esta
+
+* O simplemente listas
+* Como esta
+
+"""
+
+texto = st.text_input('Introduzca su texto')
+slider = st.slider('Mira qué slider guapo', min_value=23, max_value=73)
+checkbox = st.checkbox('Quiero vender mi alma al BBVA')
+
+desplegable = st.selectbox(
+    'También se pueden poner desplegable', 
+    ('uno', 'dos', 'tres'))
+st.write('elegiste:', desplegable)
