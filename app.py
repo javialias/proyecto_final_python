658d8872727ccad2d699f0cc2ac7a565218183bb
javialias <fjavieralias@gmail.com>
2022-12-28 13:04:58 +0100

    le he metido un menú to guapo

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import crypto_pair as cp
 
-"""
-# Proyecto final Python
-##### Samuel Medina Gutiérrez y Francisco Javier Alías Carrascosa
+with st.sidebar:
+    selected = option_menu(
+        menu_title = "Menú",
+        options = ["Aplicación", "Informe"],
+        icons = ["bar-chart-line-fill", "book"],
+        menu_icon = "cast"
+    )
 
-Este es el informe sobre el proyecto de la asignatura _Python para el análisis de Datos_ del
-Máster en Big Data Science de la Universidad de Navarra. 
-"""
+if selected == "Aplicación":
+    """
+    # Proyecto final Python
+    ##### Samuel Medina Gutiérrez y Francisco Javier Alías Carrascosa
 
+    Este es el informe sobre el proyecto de la asignatura _Python para el análisis de Datos_ del
+    Máster en Big Data Science de la Universidad de Navarra. 
+    """
+    par = cp.initialize()
+    if par:
+        st.write(f'Par seleccionado: {par.pair}')
 
+    """
+    Escoja los indicadores técnicos que desea graficar:
+    """
+    check_mediamovil = st.checkbox('Media Móvil')
+    check_rsi = st.checkbox('RSI')
+    check_media_cotiz = st.checkbox('Media Móvil junto con la cotización del par')
+
+    if st.button('Plot'):
+        if check_mediamovil:
+            st.write('media movil')
+        if check_rsi:
+            st.write('rsi')
+        if check_media_cotiz:
+            st.write('media cotizacion')
+
+if selected == "Informe":
+    st.write("esto es un informe to guapo")
+    
+    
 #1. Puedo escribir enumeraciones
 #2. Como esta
 
@@ -25,26 +56,3 @@ Máster en Big Data Science de la Universidad de Navarra.
 #    'También se pueden poner desplegable', 
 #    ('uno', 'dos', 'tres'))
 #st.write('elegiste:', desplegable)
-
-par = cp.initialize()
-if par:
-    st.write(f'Par seleccionado: {par.pair}')
-
-"""
-Escoja los indicadores técnicos que desea graficar:
-"""
-check_mediamovil = st.checkbox('Media Móvil')
-check_rsi = st.checkbox('RSI')
-check_media_cotiz = st.checkbox('Media Móvil junto con la cotización del par')
-
-
-if st.button('Plot'):
-    if check_mediamovil:
-        st.write('media movil')
-    if check_rsi:
-        st.write('rsi')
-    if check_media_cotiz:
-        st.write('media cotizacion')
-    
-    
-    
